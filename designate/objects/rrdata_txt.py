4056762beb34acb471d74940a2babe7ec0088319
Kiran Pawar <kiran.pawar@sap.com>
2022-04-20 12:08:10 +0000

    Revert "Allow TXT record over 255 characters if split"
    Since we already support more than 255 characters TXT record, this
    change breaks the designate TXT records on xena-m3.
    
    This reverts commit 5e9cca1b02a8e115e0f4cb471b180427db4c3993.

--- designate/objects/rrdata_txt.py
+++ designate/objects/rrdata_txt.py
@@ -26,23 +26,14 @@ class TXT(Record):
     Defined in: RFC1035
     """
     fields = {
-        'txt_data': fields.TxtField()
+        'txt_data': fields.TxtField(maxLength=255)
     }
 
     def _to_string(self):
         return self.txt_data
 
-    @staticmethod
-    def _is_wrapped_in_double_quotes(value):
-        return value.startswith('"') and value.endswith('"')
-
-    def _validate_record_single_string(self, value):
-        if len(value) > 255:
-            err = ("Any TXT record string exceeding "
-                   "255 characters has to be split.")
-            raise InvalidObject(err)
-
-        if not self._is_wrapped_in_double_quotes(value):
+    def _from_string(self, value):
+        if (not value.startswith('"') and not value.endswith('"')):
             # value with spaces should be quoted as per RFC1035 5.1
             for element in value:
                 if element.isspace():
@@ -58,29 +49,6 @@ class TXT(Record):
                         err = ("Quotation marks should be escaped with "
                                "backslash.")
                         raise InvalidObject(err)
-
-    def _from_string(self, value):
-        if len(value) > 255:
-            # expecting record containing multiple strings as
-            # per rfc7208 3.3 and rfc1035 3.3.14
-            stripped_value = value.strip('"')
-            if (not self._is_wrapped_in_double_quotes(value) and
-                    '" "' not in stripped_value):
-                err = ("TXT record strings over 255 characters "
-                       "have to be split into multiple strings "
-                       "wrapped in double quotes.")
-                raise InvalidObject(err)
-
-            record_strings = stripped_value.split('" "')
-            for record_string in record_strings:
-                # add back the delimiting quotes after
-                # strip and split for each string
-                record_string = '"{}"'.format(record_string)
-                # further validate each string individually
-                self._validate_record_single_string(value=record_string)
-        else:
-            # validate single TXT record string
-            self._validate_record_single_string(value=value)
 
         self.txt_data = value
 
