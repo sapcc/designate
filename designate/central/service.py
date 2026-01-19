6ca8d734c8a09bfd2e5787cfa248f927733fb559
Mikhail Samoylov <mikhailsamoiloff@gmail.com>
2026-01-19 20:08:22 +0700

    Add some assumptions
    Change-Id: I9cc4c5ba2dad1f3a3d9dbeeb086a8f014ef8cf04

--- designate/central/service.py
+++ designate/central/service.py
@@ -2587,31 +2587,19 @@ class Service(service.RPCService):
     @rpc.expected_exceptions()
     def get_pool(self, context, pool_id):
         pool = self.storage.get_pool(context, pool_id)
-
         pool_shared = False
-        if (context.domain_id != pool.domain_id) and not context.all_tenants:
+        if context.domain_id and (context.domain_id != pool.domain_id):
             pool_shared = self.storage.is_pool_shared_with_domain(
                 pool_id, context.domain_id)
             if not pool_shared:
                 raise exceptions.PoolNotFound(
-                    "Could not find pool %s" % pool.obj_name())
-
-        # TODO(johnsom) This should account for all-projects context
-        # it passes today due to ADMIN
-        if policy.enforce_new_defaults():
-            target = {
-                'pool_id': pool_id,
-                'pool_name': pool.name,
-                'pool_shared': pool_shared,
-                constants.RBAC_DOMAIN_ID: pool.domain_id
-            }
-        else:
-            target = {
-                'pool_id': pool_id,
-                'pool_name': pool.name,
-                'pool_shared': pool_shared,
-                'domain_id': pool.domain_id
-            }
+                    "Pool isn't allowed for domain %r: %r" % (context.domain_id, pool))
+        target = {
+            'pool_id': pool_id,
+            'pool_name': pool.name,
+            'pool_shared': pool_shared,
+            constants.RBAC_DOMAIN_ID: pool.domain_id
+        }
         policy.check('get_pool', context, target)
         return pool
 
